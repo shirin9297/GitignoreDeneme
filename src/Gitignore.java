0d93216131e1a6769b9fd57d2a271bcb7e79dcbb
Shirin Geldiyeva <sarahlinda1819@gmail.com>
2022-08-16 23:10:07 +0300

    C.1

--- src/Gitignore.java
+++ src/Gitignore.java
@@ -4,6 +4,6 @@ public class Gitignore {
         System.out.println("Gitignore denem 1");
         System.out.println("Gitignore deneme branch");
         System.out.println("Gitignore denem 2");
-
+        System.out.println("Gitignore denem master");
     }
 }
